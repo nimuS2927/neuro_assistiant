c9f3dafac00144ab610a6df4a74ee974bee87ae7
Ivan Sumin <sumin.ivan01@mail.ru>
2024-10-23 03:20:34 +1000

    feat: add localization for conversation (en, ru)

--- model/conversation.py
+++ model/conversation.py
@@ -3,7 +3,7 @@ from typing import Dict
 MODELS_CONFIG = {
     "IlyaGusev/saiga_mistral_7b": {
         "default_message_template": "<s>{role}\n{content}</s>",
-        "default_response_template": "<s>bot\n",
+        "default_response_template": "<s{role}\n",
         "default_system_prompt": """
                                 Ты — Сайга, русскоязычный автоматический ассистент.
                                  Ты разговариваешь с людьми и помогаешь им.
@@ -11,30 +11,41 @@ MODELS_CONFIG = {
     },
     "IlyaGusev/saiga_mistral_7b_gguf": {
         "default_message_template": "<s>{role}\n{content}</s>",
-        "default_response_template": "<s>bot\n",
+        "default_response_template": "<s>{role}\n",
         "default_system_prompt": """Ты — Сайга, русскоязычный автоматический ассистент.
 Ты разговариваешь с людьми и помогаешь им.
 Думай шаг за шагом, чтобы быть уверенным в своем ответе.""",
     },
     "ai-forever/FRED-T5-1.7B": {
         "default_message_template": "<s>{role}\n{content}</s>",
-        "default_response_template": "<s>bot\n",
+        "default_response_template": "<s>{role}\n",
         "default_system_prompt": "Ты — персональный ассистент помощник, который называется НейроЛаборант отвечающий "
         "на вопросы пользователя,",
     },
 }
 
+LANGUAGES = ["en", "ru"]
+LANGUAGES_VOCAB = {
+    "en": {
+        "user": "user",
+        "assistant": "assistant",
+        "system": "system",
+        "document": "document",
+    },
+    "ru": {
+        "user": "пользователь",
+        "assistant": "ассистент",
+        "system": "инструкция",
+        "document": "документ",
+    },
+}
 
 # Думай шаг за шагом и используй Documents для точного ответа на вопросы пользователя.
 # Если ты не нашел ответа в документах, то прямо об этом и скажи.
 
 
 class Conversation:
-    def __init__(
-        self,
-        model_name: str,
-        config: Dict = None,
-    ):
+    def __init__(self, model_name: str, config: Dict = None, language: str = "en"):
         if config is None:
             config = MODELS_CONFIG
         self.model_name = model_name
@@ -45,49 +56,45 @@ class Conversation:
             "default_response_template"
         )
         self.default_system_prompt = config.get(model_name).get("default_system_prompt")
+        self.language = language.lower() if language.lower() in LANGUAGES else "en"
+        self.language_vocab = LANGUAGES_VOCAB[self.language]
         self.messages = [{"role": "system:", "content": self.default_system_prompt}]
 
     def add_user_message(self, message):
-        self.messages.append({"role": "user", "content": message})
+        self.messages.append({"role": self.language_vocab["user"], "content": message})
 
     def add_bot_message(self, message):
-        self.messages.append({"role": "assistant", "content": message})
+        self.messages.append(
+            {"role": self.language_vocab["assistant"], "content": message}
+        )
 
     def add_documents(self, content):
-        self.messages.append({"role": "Documents", "content": content})
+        self.messages.append(
+            {"role": self.language_vocab["document"], "content": content}
+        )
 
     def get_system_prompt(self):
         return self.default_message_template.format(
-            role="system", content=self.default_system_prompt
+            role=self.language_vocab["system"], content=self.default_system_prompt
         )
 
-    def get_prompt(self):
+    def get_prompt(
+        self,
+        with_system: bool = True,
+        delete_documents: bool = False,
+    ):
         final_text = ""
-        len_messages = len(self.messages)
-        documents_context = None
-        if self.messages[-1].get("role") == "Documents":
-            last = len_messages - 1
-            documents_context = self.messages.pop(-1)
-        else:
-            last = len_messages
-
-        for i, message in enumerate(self.messages[:last]):
-            if i == last - 1 and documents_context:
-                content = message.get("content")
-                content += f"\nDocuments\n{documents_context.get('content')}"
-                message_text = self.default_message_template.format(
-                    role="User", content=content
-                )
-                final_text += message_text
-                # else:
-                #     content = message.get("content")
-                #     content += f"\nDocuments\nОтсутствуют"
-                #     message_text = self.default_message_template.format(
-                #         role="User", content=content
-                #     )
-                #     final_text += message_text
-            else:
-                message_text = self.default_message_template.format(**message)
-                final_text += message_text
-        final_text += self.default_response_template
+        for message in self.messages:
+            if message["role"] == "system" and not with_system:
+                continue
+            final_text += self.default_message_template.format(**message)
+        final_text += self.default_response_template.format(
+            self.language_vocab["assistant"]
+        )
+        if delete_documents:
+            self.messages = [
+                message
+                for message in self.messages
+                if not message["role"] == self.language_vocab["document"]
+            ]
         return final_text.strip()
